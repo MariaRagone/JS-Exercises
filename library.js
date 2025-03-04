539359c6338a890078e3e511e9c27a408c4a5a8e
mragone <maria.ragone@samaritanministries.org>
2025-03-04 16:54:20 -0500

    library exercise

---
+++ library.js
@@ -0,0 +1,23 @@
+const bookName = [
+  "A Song of Ice and Fire",
+  "The Great Gatsby",
+  "Crime & Punishment",
+  "Great Expectations",
+  "You Don't Know JS",
+];
+const favoriteBooks = [];
+console.log(bookName.length);
+
+const addFavoriteBook = (bookName) => {
+  for (let i = 0; i < bookName.length; i++) {
+    if (bookName[i].includes("Great")) {
+    } else {
+      favoriteBooks.push(bookName[i]);
+    }
+    console.log(bookName[i]);
+  }
+};
+
+addFavoriteBook(bookName);
+console.log(favoriteBooks.length);
+console.log(favoriteBooks);
